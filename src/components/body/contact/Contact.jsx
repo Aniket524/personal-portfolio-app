21dab5533997198a8bc1820fd915b2d379e16ff7
Aniket524 <81089019+Aniket524@users.noreply.github.com>
2023-08-03 06:21:18 +0000

    Resume Button Deownload Imported

--- src/components/body/contact/Contact.jsx
+++ src/components/body/contact/Contact.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import "./contact.css"
 import Separator from '../../common/separater/Separator';
-import SocialContct from '../../common/social-contact/SocialContact'
+import SocialContct from '../../common/social-contact/SocialContact';
+import resume from '../../../assets/AniketKarpe_resume.pdf';
 
 const Contact = () => {
   return <div className='contact'>
@@ -13,7 +14,13 @@ const Contact = () => {
         <SocialContct />
       </div>
       <div className="download">
-        <a download href={require('../../../assets/AniketKarpe_resume.pdf').default}>
+      <a
+        href={resume}
+        download="Aniket Karpe Resume"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {/* <a download href={require('../../../assets/AniketKarpe_resume.pdf').default}> */}
           <i class="fi fi-rr-download"></i>
           Download Resume
         </a>
